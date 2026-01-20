e4f97a46603ab668b5286f6aec7a2fce02ca3dbe
Maksims Turs <maksims07turs@gmail.com>
2026-01-20 15:26:54 +0100

    Change: migrated to new version of colorizer.js.

--- src/utils/colorize-log-level.util.ts
+++ src/utils/colorize-log-level.util.ts
@@ -5,11 +5,11 @@ import colorizer from "colorizer.js";
 export default function colorizeLogLevel(level: string, text: string): string {
   switch(level) {
     case STRING.LOG_LEVEL.ERROR:
-      return colorizer().bold().bg().rgb(255, 0, 0).text(text);
+      return colorizer().bold().font().rgb(255, 0, 0).text(text);
     case STRING.LOG_LEVEL.INFO:
-      return colorizer().bold().bg().rgb(0, 0, 255).text(text);
+      return colorizer().bold().font().rgb(0, 0, 255).text(text);
     case STRING.LOG_LEVEL.WARN:
-      return colorizer().bold().bg().rgb(255, 255, 0).text(text);
+      return colorizer().bold().font().rgb(255, 255, 0).text(text);
     default:
       return text;
   }
